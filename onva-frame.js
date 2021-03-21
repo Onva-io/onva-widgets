3586b523d42d39bd7d98bfec6fe69130b1c3c721
Matt Wilson <codebrewery@gmail.com>
2021-03-21 13:24:04 +0000

    much to do but getting there

--- onva-frame.js
+++ onva-frame.js
@@ -1,121 +1,122 @@
-function init() {
-    var removeFrame = function() {
-        var frame = document.getElementById('onva-frame-wrapper');
+(function () {
+    var script = document.currentScript;
+    var containerId = script.getAttribute('data-container-id') || 'onva-frame';
+    var surveyId = script.getAttribute('data-survey-uuid');
+    var defer = ((script.getAttribute('data-defer') || '') == 'true');
+    var triggerId = script.getAttribute('data-trigger-id');
 
-        if (frame) {
-            document.body.removeChild(frame);
+    if (!containerId) {
+        console.error('No container ID specified, will not be able to render survey - use data-container-id on script tag');
+
+    } else if (!surveyId) {
+        console.error('No survey ID specified, will not be able to render survey - use data-survey-uuid on script tag');
+
+    } else {
+        var identifier = script.getAttribute('data-identifier');
+        var metadata = script.getAttribute('data-metadata');
+        var locale = script.getAttribute('data-locale') || document.getElementsByTagName('html')[0].lang || 'en';
+        var redirect = script.getAttribute('data-redirect-uri');
+
+        if (metadata != "") {
+            metadata = JSON.parse(metadata);
         }
-    };
 
-    var complete = function() {
-        console.log('survey complete');
+        var onva_js = script.src.replace(/\/[\w-]+((\.min)?\.js)$/, "/onva\$1");
+        var onva_css = script.src.replace(/\.js$/, '.css');
 
-        if (redirect) {
-            top.location.href = redirect;
-        } else {
-            removeFrame();
-        }
-    };
+        document.addEventListener('DOMContentLoaded', function() {
+            function init() {
+                var removeFrame = function() {
+                    var frame = document.getElementById('onva-frame-wrapper');
 
-    var postRender = function() {
-        console.log('survey rendered');
-    };
+                    if (frame) {
+                        document.body.removeChild(frame);
+                    }
+                };
 
-    var error = function(data) {
-        console.error('survey error');
-        console.error(data);
-    };
+                var complete = function() {
+                    console.log('survey complete');
 
-    var preRender = function() {
-        var frame = `
-        <div id="onva-frame-wrapper">
-            <div class="onva-frame-wrapper">
-                <a href="javascript:void(0);" id="onva-frame-closer">
-                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="503.021px" height="503.021px" viewBox="0 0 503.021 503.021" style="enable-background:new 0 0 503.021 503.021;" xml:space="preserve"> <g> <path d="M491.613,75.643l-64.235-64.235c-15.202-15.202-39.854-15.202-55.056,0L251.507,132.222L130.686,11.407 c-15.202-15.202-39.853-15.202-55.055,0L11.401,75.643c-15.202,15.202-15.202,39.854,0,55.056l120.821,120.815L11.401,372.328 c-15.202,15.202-15.202,39.854,0,55.056l64.235,64.229c15.202,15.202,39.854,15.202,55.056,0l120.815-120.814l120.822,120.814 c15.202,15.202,39.854,15.202,55.056,0l64.235-64.229c15.202-15.202,15.202-39.854,0-55.056L370.793,251.514l120.82-120.815 C506.815,115.49,506.815,90.845,491.613,75.643z"/> </g> </svg>
-                </a>
-                <div class="onva-frame" id="` + containerId + `">
-                </div>
-            </div>
-        </div>
-        `;
+                    if (redirect) {
+                        top.location.href = redirect;
+                    } else {
+                        removeFrame();
+                    }
+                };
 
-        document.body.innerHTML += frame;
+                var postRender = function() {
+                    console.log('survey rendered');
+                };
 
-        document.getElementById('onva-frame-closer').addEventListener('click', removeFrame);
-    };
+                var error = function(data) {
+                    console.error('survey error');
+                    console.error(data);
+                };
 
-    window._onva_framed = new Survey(containerId, surveyId, complete, locale, identifier, metadata);
+                var preRender = function() {
+                    var frame = `
+                    <div id="onva-frame-wrapper">
+                        <div class="onva-frame-wrapper">
+                            <a href="javascript:void(0);" id="onva-frame-closer">
+                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="503.021px" height="503.021px" viewBox="0 0 503.021 503.021" style="enable-background:new 0 0 503.021 503.021;" xml:space="preserve"> <g> <path d="M491.613,75.643l-64.235-64.235c-15.202-15.202-39.854-15.202-55.056,0L251.507,132.222L130.686,11.407 c-15.202-15.202-39.853-15.202-55.055,0L11.401,75.643c-15.202,15.202-15.202,39.854,0,55.056l120.821,120.815L11.401,372.328 c-15.202,15.202-15.202,39.854,0,55.056l64.235,64.229c15.202,15.202,39.854,15.202,55.056,0l120.815-120.814l120.822,120.814 c15.202,15.202,39.854,15.202,55.056,0l64.235-64.229c15.202-15.202,15.202-39.854,0-55.056L370.793,251.514l120.82-120.815 C506.815,115.49,506.815,90.845,491.613,75.643z"/> </g> </svg>
+                            </a>
+                            <div class="onva-frame" id="` + containerId + `">
+                            </div>
+                        </div>
+                    </div>
+                    `;
 
-    window._onva_framed.showFrame = function() {
-        window._onva_framed.begin(preRender, postRender, error);
-    };
+                    var doc = document.createElement('div');
+                    doc.innerHTML = frame;
+                    document.body.appendChild(doc.children[0]);
 
-    if (triggerId) {
-        document.getElementById(triggerId).addEventListener('click', window._onva_framed.showFrame);
-    }
+                    document.getElementById('onva-frame-closer').addEventListener('click', removeFrame);
+                    document.getElementById('onva-frame-wrapper').addEventListener('click', removeFrame);
+                    document.getElementById('onva-frame').addEventListener('click', function (e) { e.stopPropagation() });
+                };
 
-    window._onva_framed.go = function() {
-        window._onva_framed.showFrame();
-    };
+                window._onva_framed = new Survey(containerId, surveyId, complete, locale, identifier, metadata);
 
-    if (!defer) {
-        window._onva_framed.go();
-    }
-}
+                window._onva_framed.go = function() {
+                    window._onva_framed.begin(preRender, postRender, error);
+                };
 
-var script = document.currentScript;
-var containerId = script.getAttribute('data-container-id') || 'onva-frame';
-var surveyId = script.getAttribute('data-survey-uuid');
-var defer = ((script.getAttribute('data-defer') || '') == 'true');
-var triggerId = ((script.getAttribute('data-trigger-id') || '') == 'true');
+                if (triggerId) {
+                    document.getElementById(triggerId).addEventListener('click', window._onva_framed.go);
+                }
 
-if (!containerId) {
-    console.error('No container ID specified, will not be able to render survey - use data-container-id on script tag');
-
-} else if (!surveyId) {
-    console.error('No survey ID specified, will not be able to render survey - use data-survey-uuid on script tag');
-
-} else {
-    var identifier = script.getAttribute('data-identifier');
-    var metadata = script.getAttribute('data-metadata');
-    var locale = script.getAttribute('data-locale') || document.getElementsByTagName('html')[0].lang || 'en';
-    var redirect = script.getAttribute('data-redirect-uri');
-
-    if (metadata != "") {
-        metadata = JSON.parse(metadata);
-    }
-
-    var onva_js = script.src.replace(/\/[\w-]+((\.min)?\.js)$/, "/onva\$1");
-    var onva_css = script.src.replace(/\.js$/, '.css');
-
-    window.addEventListener('load', function() {
-        var style = document.createElement('link');
-        style.type = 'text/css';
-        style.rel = 'stylesheet';
-        style.media = 'screen';
-        style.async = true;
-        style.href = onva_css;
-        (document.head || document.body).appendChild(style);
-
-        // check if we've already loaded the script
-        let loaded = false;
-
-        for (var i = 0; i < document.scripts.length; ++i) {
-            if (document.scripts[i].src == onva_js) {
-                loaded = true;
-                break;
+                if (!defer) {
+                    window._onva_framed.go();
+                }
             }
-        }
 
-        if (loaded) {
-            init();
-        } else {
-            var script = document.createElement('script');
-            script.async = true;
-            script.addEventListener('load', init, false);
-            script.src = onva_js;
-            document.body.appendChild(script);
-        }
-    });
-}
+            var style = document.createElement('link');
+            style.type = 'text/css';
+            style.rel = 'stylesheet';
+            style.media = 'screen';
+            style.async = true;
+            style.href = onva_css;
+            (document.head || document.body).appendChild(style);
 
+            // check if we've already loaded the script
+            let loaded = false;
+
+            for (var i = 0; i < document.scripts.length; ++i) {
+                if (document.scripts[i].src == onva_js) {
+                    loaded = true;
+                    break;
+                }
+            }
+
+            if (loaded) {
+                init();
+            } else {
+                var script = document.createElement('script');
+                script.async = true;
+                script.addEventListener('load', init, false);
+                script.src = onva_js;
+                document.body.appendChild(script);
+            }
+        });
+    }
+})();
--- onva-nps.js
+++ onva-nps.js
@@ -1,160 +1,162 @@
-function init() {
-    var element = `
-        <div id="onva-nps-wrapper">
-            <div class="onva-nps-wrapper">
-                <a href="javascript:void(0);" id="onva-frame-closer">
-                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="503.021px" height="503.021px" viewBox="0 0 503.021 503.021" style="enable-background:new 0 0 503.021 503.021;" xml:space="preserve"> <g> <path d="M491.613,75.643l-64.235-64.235c-15.202-15.202-39.854-15.202-55.056,0L251.507,132.222L130.686,11.407 c-15.202-15.202-39.853-15.202-55.055,0L11.401,75.643c-15.202,15.202-15.202,39.854,0,55.056l120.821,120.815L11.401,372.328 c-15.202,15.202-15.202,39.854,0,55.056l64.235,64.229c15.202,15.202,39.854,15.202,55.056,0l120.815-120.814l120.822,120.814 c15.202,15.202,39.854,15.202,55.056,0l64.235-64.229c15.202-15.202,15.202-39.854,0-55.056L370.793,251.514l120.82-120.815 C506.815,115.49,506.815,90.845,491.613,75.643z"/> </g> </svg>
-                </a>
-                <div class="onva-nps" id="` + containerId + `">
-                </div>
-            </div>
-        </div>
-    `;
+(function () {
+    var script = document.currentScript;
+    var containerId = script.getAttribute('data-container-id') || 'onva-nps';
+    var surveyId = script.getAttribute('data-survey-uuid');
+    var defer = ((script.getAttribute('data-defer') || '') == 'true');
 
-    var preRender = function() {
-        var doc = document.createElement('div');
-        doc.innerHTML = element;
-        document.body.appendChild(doc.children[0]);
+    if (!surveyId) {
+        console.error('No survey ID specified, will not be able to render survey - use data-survey-uuid on script tag');
 
-        document.getElementById('onva-frame-closer').addEventListener('click', removeFrame);
-    };
+    } else {
+        var identifier = script.getAttribute('data-identifier');
+        var metadata = script.getAttribute('data-metadata');
+        var locale = script.getAttribute('data-locale') || document.getElementsByTagName('html')[0].lang || 'en';
 
-    // create container
-    var removeFrame = function() {
-        var frame = document.getElementById('onva-nps-wrapper');
-
-        if (frame) {
-            document.body.removeChild(frame);
+        if (metadata != "") {
+            metadata = JSON.parse(metadata);
         }
-    };
 
-    var complete = function() {
-        removeFrame();
-    };
+        var onva_js = script.src.replace(/\/[\w-]+((\.min)?\.js)$/, "/onva\$1");
+        var onva_css = script.src.replace(/\.js$/, '.css');
 
-    var postRender = function() {
-        var buttons = document.getElementsByClassName('onva-nps-button');
+        document.addEventListener('DOMContentLoaded', function() {
+            function init() {
+                var element = `
+                    <div id="onva-nps-wrapper">
+                        <div class="onva-nps-wrapper">
+                            <a href="javascript:void(0);" id="onva-frame-closer">
+                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="503.021px" height="503.021px" viewBox="0 0 503.021 503.021" style="enable-background:new 0 0 503.021 503.021;" xml:space="preserve"> <g> <path d="M491.613,75.643l-64.235-64.235c-15.202-15.202-39.854-15.202-55.056,0L251.507,132.222L130.686,11.407 c-15.202-15.202-39.853-15.202-55.055,0L11.401,75.643c-15.202,15.202-15.202,39.854,0,55.056l120.821,120.815L11.401,372.328 c-15.202,15.202-15.202,39.854,0,55.056l64.235,64.229c15.202,15.202,39.854,15.202,55.056,0l120.815-120.814l120.822,120.814 c15.202,15.202,39.854,15.202,55.056,0l64.235-64.229c15.202-15.202,15.202-39.854,0-55.056L370.793,251.514l120.82-120.815 C506.815,115.49,506.815,90.845,491.613,75.643z"/> </g> </svg>
+                            </a>
+                            <div class="onva-nps" id="` + containerId + `">
+                            </div>
+                        </div>
+                    </div>
+                `;
 
-        for (var i = 0; i < buttons.length; ++i) {
-            buttons[i].addEventListener('click', function () {
-                setTimeout(function () { window._onva_nps.submit(); }, 100);
-            });
-        }
-    };
+                var preRender = function() {
+                    var doc = document.createElement('div');
+                    doc.innerHTML = element;
+                    document.body.appendChild(doc.children[0]);
 
-    var error = function(data) {
-        window.error('error');
-        window.error(data);
-    };
+                    document.getElementById('onva-frame-closer').addEventListener('click', removeFrame);
+                };
 
-    window._onva_nps = new Survey(containerId, surveyId, complete, locale, identifier, metadata);
+                // create container
+                var removeFrame = function() {
+                    var frame = document.getElementById('onva-nps-wrapper');
 
-    /*
-    window._onva_nps.answerTemplate = `
-        <li>
-            <input class="onva-nps-answer" value="{{ value }}"
-        </li>
-    `;
+                    if (frame) {
+                        document.body.removeChild(frame);
+                    }
+                };
 
-    window._onva_nps.wrapperTemplate = `
-        <div class="onva-nps-wrapper" style="margin: 0 auto;">
-            <p class="onva-nps-question">{{ content }}</p>
-            <ul class="onva-nps-answers">
-                <!-- answers will be inserted here -->
-            </ul>
-        </div>
-    `;
-    */
+                var complete = function() {
+                    removeFrame();
+                };
 
-    window._onva_nps.answerTemplate = `
-        <div class="onva-answer-wrapper" data-answer-id="{{ answer_id }}">
-            <!-- answer will be injected here -->
-        </div>
-    `;
+                var postRender = function() {
+                    var buttons = document.getElementsByClassName('onva-nps-button');
 
-    window._onva_nps.surveyTemplate = `
-        <div class="onva-survey" data-survey-id="{{ survey_uuid }}" lang="{{ locale }}" dir="{{ text_direction }}">
-            <h4>{{ title }}</h4>
-            
-            <div class="onva-questions-container">
-                <!-- questions will be injected here -->
-            </div>
-        </div>
-    `;
+                    for (var i = 0; i < buttons.length; ++i) {
+                        buttons[i].addEventListener('click', function () {
+                            setTimeout(function () { window._onva_nps.submit(); }, 100);
+                        });
+                    }
+                };
 
-    window._onva_nps.questionTemplate = `
-        <div class="onva-question" data-question-id="{{ question_id }}">
-            <div class="onva-error" style="display: none;"></div>
+                var error = function(data) {
+                    window.error('error');
+                    window.error(data);
+                };
 
-            <div class="onva-answers-container">
-                <!-- answers will be inserted here -->
-            </div>
-        </div>
-    `;
+                window._onva_nps = new Survey(containerId, surveyId, complete, locale, identifier, metadata);
 
-    window._onva_nps.radioTemplate = `
-        <div class="onva-nps-entry">
-            <input type="radio" name="{{ name }}" id="{{ id }}" value="{{ value }}" class="onva-answer" />
-            <label class="onva-nps-button" for="{{ id }}">{{ content }}</label>
-        </div>
-    `;
+                /*
+                window._onva_nps.answerTemplate = `
+                    <li>
+                        <input class="onva-nps-answer" value="{{ value }}"
+                    </li>
+                `;
 
-    window._onva_nps.go = function() {
-        window._onva_nps.begin(preRender, postRender, error);
-    };
+                window._onva_nps.wrapperTemplate = `
+                    <div class="onva-nps-wrapper" style="margin: 0 auto;">
+                        <p class="onva-nps-question">{{ content }}</p>
+                        <ul class="onva-nps-answers">
+                            <!-- answers will be inserted here -->
+                        </ul>
+                    </div>
+                `;
+                */
 
-    if (!defer) {
-        window._onva_nps.go();
-    }
-}
+                window._onva_nps.answerTemplate = `
+                    <div class="onva-answer-wrapper" data-answer-id="{{ answer_id }}">
+                        <!-- answer will be injected here -->
+                    </div>
+                `;
 
-var script = document.currentScript;
-var containerId = script.getAttribute('data-container-id') || 'onva-nps';
-var surveyId = script.getAttribute('data-survey-uuid');
-var defer = ((script.getAttribute('data-defer') || '') == 'true');
+                window._onva_nps.surveyTemplate = `
+                    <div class="onva-survey" data-survey-id="{{ survey_uuid }}" lang="{{ locale }}" dir="{{ text_direction }}">
+                        <h4>{{ title }}</h4>
+                        
+                        <div class="onva-questions-container">
+                            <!-- questions will be injected here -->
+                        </div>
+                    </div>
+                `;
 
-if (!surveyId) {
-    console.error('No survey ID specified, will not be able to render survey - use data-survey-uuid on script tag');
+                window._onva_nps.questionTemplate = `
+                    <div class="onva-question" data-question-id="{{ question_id }}">
+                        <div class="onva-error" style="display: none;"></div>
 
-} else {
-    var identifier = script.getAttribute('data-identifier');
-    var metadata = script.getAttribute('data-metadata');
-    var locale = script.getAttribute('data-locale') || document.getElementsByTagName('html')[0].lang || 'en';
+                        <div class="onva-answers-container">
+                            <!-- answers will be inserted here -->
+                        </div>
+                    </div>
+                `;
 
-    if (metadata != "") {
-        metadata = JSON.parse(metadata);
-    }
+                window._onva_nps.radioTemplate = `
+                    <div class="onva-nps-entry">
+                        <input type="radio" name="{{ name }}" id="{{ id }}" value="{{ value }}" class="onva-answer" />
+                        <label class="onva-nps-button" for="{{ id }}">{{ content }}</label>
+                    </div>
+                `;
 
-    var onva_js = script.src.replace(/\/[\w-]+((\.min)?\.js)$/, "/onva\$1");
-    var onva_css = script.src.replace(/\.js$/, '.css');
+                window._onva_nps.go = function() {
+                    window._onva_nps.begin(preRender, postRender, error);
+                };
 
-    window.addEventListener('load', function() {
-        var style = document.createElement('link');
-        style.type = 'text/css';
-        style.rel = 'stylesheet';
-        style.media = 'screen';
-        style.async = true;
-        style.href = onva_css;
-        (document.head || document.body).appendChild(style);
-
-        // check if we've already loaded the script
-        let loaded = false;
-
-        for (var i = 0; i < document.scripts.length; ++i) {
-            if (document.scripts[i].src == onva_js) {
-                loaded = true;
-                break;
+                if (!defer) {
+                    window._onva_nps.go();
+                }
             }
-        }
 
-        if (loaded) {
-            init();
-        } else {
-            var script = document.createElement('script');
-            script.async = true;
-            script.addEventListener('load', init, false);
-            script.src = onva_js;
-            document.body.appendChild(script);
-        }
-    });
-}
+            var style = document.createElement('link');
+            style.type = 'text/css';
+            style.rel = 'stylesheet';
+            style.media = 'screen';
+            style.async = true;
+            style.href = onva_css;
+            (document.head || document.body).appendChild(style);
+
+            // check if we've already loaded the script
+            let loaded = false;
+
+            for (var i = 0; i < document.scripts.length; ++i) {
+                if (document.scripts[i].src == onva_js) {
+                    loaded = true;
+                    break;
+                }
+            }
+
+            if (loaded) {
+                init();
+            } else {
+                var script = document.createElement('script');
+                script.async = true;
+                script.addEventListener('load', init, false);
+                script.src = onva_js;
+                document.body.appendChild(script);
+            }
+        });
+    }
+})();
